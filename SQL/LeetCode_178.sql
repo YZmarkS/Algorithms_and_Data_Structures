1cc986f83aee150633b2c3ca7d0c563dd80be9e3
M <38927629+YZmarkS@users.noreply.github.com>
2026-09-06 18:18:32 -0400

    update

--- SQL/LeetCode_178.sql
+++ SQL/LeetCode_178.sql
@@ -1,8 +1,8 @@
 WITH ScoreRank AS (
-    SELECT score, ROW_NUMBER() OVER(ORDER BY score DESC) AS r
+    SELECT score, ROW_NUMBER() OVER(ORDER BY score DESC) AS `rank`
     FROM Scores
     GROUP BY score
 )
-SELECT Scores.score, r as `rank`
+SELECT Scores.score, `rank`
 FROM Scores JOIN ScoreRank ON Scores.score = ScoreRank.score
 ORDER BY Scores.score DESC
